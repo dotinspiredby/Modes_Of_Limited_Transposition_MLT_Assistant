0f16383d9cb79bdd8469a7e2eb79b1e47705d6b8
dotinspiredby <81487419+dotinspiredby@users.noreply.github.com>
2021-07-27 03:35:03 +0300

    Update for_notation.py

--- for_notation.py
+++ for_notation.py
@@ -4,43 +4,37 @@ from music21 import converter, metadata, serial
 
 def notate_instr2(integerslist, index, modeid):
     notes = serial.ToneRow(integerslist).noteNames()
-    parameters = '%s/4' % len(notes)
+    parameters = ['%s/4' % len(notes)]
     for g in range(len(notes)):
         if int(integerslist[g]) < int(index):
             if notes[g][-1] in "#-":
-                notes[g] = str(notes[g]).lower()[0] + "'" + str(notes[g]).lower()[-1] + "4"
+                parameters.append(str(notes[g]).lower()[0] + "'" + str(notes[g]).lower()[-1] + "4")
             else:
-                notes[g] = str(notes[g]).lower() + "'4"
+                parameters.append(str(notes[g]).lower() + "'4")
         else:
-            notes[g] = str(notes[g]).lower() + '4'
-    notes[-1] += '_MLT%s' % modeid + 't%s ' % index
-    for g in notes:
-        parameters += '\t' + g
-    yield parameters
+            parameters.append(str(notes[g]).lower() + '4')
+    parameters[-1] += '_MLT%s' % modeid + 't%s ' % index
+
+    return '\t'.join(parameters)
 
 
-def exctract_to_str(list_int, index, mode_id):
-    indx = index
+def exctract_to_str(list_int, mode_id):
     list2 = []
-    end_of_unpacking = False
-    for i in list_int:
-        if isinstance(i, int):
-            end_of_unpacking = True
-        elif isinstance(i, tuple):
-            index_info, row = i
-            indx = index_info[-2]
-            list2 = row
-    if end_of_unpacking:
-        return notate_instr2(list_int, indx, mode_id)
-    else:
-        return exctract_to_str(list2, indx, mode_id)
+    for tup in list_int:
+        index_info, row = tup
+        indx = index_info[-2]
+        list2.append(str(notate_instr2(row, indx, mode_id)))
+
+    return ''.join(list2)
 
 
 def xml_export(row_data):
-    notation = 'tinyNotation: '
+    notatn = []
     for m in range(len(row_data)):
         if len(row_data[m]) > 0:
-            notation += str(*exctract_to_str(row_data[m], None, m + 1))
+            notatn.append(str(exctract_to_str(row_data[m], (m + 1))))
+    notation = "tinyNotation: " + ' '.join(notatn)
+
 
     if input('\n''Would you like your printed XML copy (yes/no)?') == 'yes':
         s = converter.parse(notation)
@@ -57,4 +51,6 @@ def xml_export(row_data):
 
 if __name__ == "__main__":
     xml_export([*total_check()])
+
+
 
